d198e21c7c0a1a9f6cd8f67c55297bebc6548f2b
yuki0418 <keryuuki0418@gmail.com>
2024-12-21 18:34:14 +0900

    create live and speaker view with revealjs

---
+++ src/routes/live/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import "reveal.js/dist/reveal.css";
+  import "reveal.js/dist/theme/beige.css";
+  import "reveal.js/plugin/highlight/monokai.css";
+
+  import { onMount } from "svelte";
+  import Reveal from "reveal.js";
+  import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+  import RevealNotes from "reveal.js/plugin/notes/notes.esm.js";
+  import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
+
+  let deck: Reveal.Api | undefined = $state();
+
+  onMount(() => {
+    deck = new Reveal({
+      plugins: [Markdown, RevealNotes],
+      embedded: false,
+    });
+
+    deck.initialize();
+  });
+
+  getCurrentWebviewWindow().listen<{
+    event: string;
+    payload: any;
+  }>("live", ({ event, payload }) => {
+    switch (payload.event) {
+      case "next":
+        deck?.next();
+        break;
+      case "prev":
+        deck?.prev();
+        break;
+    }
+  });
+</script>
+
+<div class="reveal">
+  <div class="slides">
+    <section>
+      <section>Slide v 1</section>
+      <section>Slide v 2</section>
+      <section>Slide v 3</section>
+    </section>
+    <section>
+      Slide 2
+
+      <aside class="notes">Shhh, these are your private notes 📝</aside>
+    </section>
+    <section>Slide 3</section>
+  </div>
+</div>
+
+<style>
+  .reveal {
+    height: 100dvh;
+    background-color: wheat;
+  }
+</style>
---
+++ src/routes/speaker-view/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { emitTo } from "@tauri-apps/api/event";
+
+  const prev = async () => {
+    await emitTo("liveViewWindow", "live", {
+      event: "prev",
+      payload: null,
+    });
+  };
+
+  const next = async () => {
+    emitTo("liveViewWindow", "live", {
+      event: "next",
+      payload: null,
+    });
+  };
+</script>
+
+<div>
+  <h1>Speaker View</h1>
+  <button onclick={prev}>prev</button>
+  <button onclick={next}>next</button>
+</div>
+
+<style></style>
